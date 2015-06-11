34cb942d1d091ef079ab15e58cebcffa3f358e43
Dan Chilton <dan.chilton@gmail.com>
2015-06-11 17:51:06 -0400

    Automatic score updating

--- app/client/js/dan.js
+++ app/client/js/dan.js
@@ -9,3 +9,35 @@ Reveal.addEventListener( 'slidechanged', function( event ) {
     audio.play();
   }
 } );
+
+Reveal.addEventListener( 'score', function( event ) {
+  var json = 'https://spreadsheets.google.com/feeds/cells/1DHiAUNEQdf-p7ZkjpQmu6KlUD1nBBGmGqCMjAWc9LQQ/od6/public/values?alt=json-in-script&callback=?';
+  $.getJSON(json).done(function(data){
+    var scores = {};
+    var scoreSort = [];
+    var name;
+    data.feed.entry.forEach(function(v, i){
+      if ( i > 6 ) {
+        var test = v.content.$t;
+        var content = parseInt(test);
+        if ( isNaN(content) ) {
+          name = test;
+          scores[name] = [];
+        } else {
+          scores[name].push(content);
+        }
+      }
+    });
+    for( name in scores ) {
+      var score = _.reduce(scores[name], function(memo, num){ return memo + num });
+      scoreSort.push({ name : name, score : score });
+    }
+    var sortedScores = _.sortBy(scoreSort, 'score');
+    sortedScores.reverse();
+    $('.live-scores').empty();
+    sortedScores.forEach(function(v){
+      $('.live-scores').append('<li><b>' + v.name + ':</b> ' + v.score + '</li>');
+    });
+  });
+
+});
